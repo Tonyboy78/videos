5341733ca72487984883a63b9a735f2939417198
Anton <tonyboy@live.se>
2021-02-02 02:25:29 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+
+const KEY = 'AIzaSyDavpsNsQzBTsVy7D9Xv36X4Ao2FL4G7lM';
+
+class App extends React.Component {
+  state = {videos: [], selectedVideo: null};
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+        part: 'snippet',
+        maxResults: 5,
+        type: 'video',
+        key: KEY,
+      },
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({selectedVideo: video});
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+export default App;
